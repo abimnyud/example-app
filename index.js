1116c01ccf34a703033a2313915cfd8bb5eb0b05
Abimanyu <hello@abimanyu.co>
2022-06-02 21:32:49 +0700

    Fix port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app = require('./src/app');
 const db = require('./connection/dbConnect');
 
-app.listen(Number(process.env.APP_PORT || 8080), async () => {
+app.listen(Number(process.env.PORT || 5000), async () => {
   try {
     await db.connect();
     console.log(`DB connected. listening on port ${process.env.APP_PORT}`);
