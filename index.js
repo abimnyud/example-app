8dd76f437a07beb0badade9b6e054e4d1f6b0af2
Abimanyu <hello@abimanyu.co>
2022-06-02 21:35:06 +0700

    Fix undefined variable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('./connection/dbConnect');
 app.listen(Number(process.env.PORT || 5000), async () => {
   try {
     await db.connect();
-    console.log(`DB connected. listening on port ${process.env.APP_PORT}`);
+    console.log(`DB connected. listening on port ${process.env.PORT}`);
   } catch (e) {
     console.log(e);
   }
