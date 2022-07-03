faeb5b5df1f306a90e1ac032e5c60704c7814113
Matahari <103297593+MatahariP@users.noreply.github.com>
2022-07-03 07:00:32 +0700

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const bukuRoutes = require('./bukuRoutes');
 const GenreRoutes = require('./GenreRoutes');
 const UserRoutes = require('./UserRoutes');
+const junctionRoutes = require('./junctionRoutes');
 
 router.use('/buku', bukuRoutes);
 router.use('/genre', GenreRoutes);
 router.use('/user', UserRoutes);
+router.use('/junction', junctionRoutes);
 
 router.all('*', (req, res) => {
     res.end('<h1>Not Found. Please refer to our documentation.</h1>');
